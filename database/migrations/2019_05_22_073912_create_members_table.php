e0894d7547dc45912e3cb5bdff02d85603e66504
Rafal Nowak <rubeus77@gmail.com>
2019-05-22 10:11:30 +0200

    utworzenie logowania oraz modelu, kontrolera i migracji dla Members

---
+++ database/migrations/2019_05_22_073912_create_members_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->bigIncrements('id');
+	    $table->string('first_name')->nullable($value = false);
+	    $table->string('last_name')->nullable($value = false);
+	    $table->string('screen_name')->nullable();
+	    $table->date('birth_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
