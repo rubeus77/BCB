295b3d09ac5ea5547d252eaf0ee09358fbf4df4b
Rafal Nowak <rubeus77@gmail.com>
2019-05-23 15:19:35 +0200

    do rozwiązania problem z logowaniem

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,78 +7,44 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button> -->
+        <header>
+            <nav class="container">
+                <div class="logo">
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">Test</li>
-                        <li class="nav-item btn btn-primary"><a class="dropdown-item btn btn-primary" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-                        </li>
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
                 </div>
-            </div>
-        </nav>
+                <ul class="menu">
+                    @guest 
+                    <li class="myButton"><a href="{{ route('login') }}">{{ __('Zaloguj') }}</a></li>
+                    <!-- FIXME: w późniejszym czasie wyczyścić możliwość rejestrowania -->
+                    @if(Route::has('register'))
+                        <li class="myButton"><a href="{{ route('register') }}">{{ __('Zarejestruj')}}
+                    @endif
+                    @else
+                    <!-- FIXME: usupełnić odpowiednie drogi dla przycisków Członkowie i Płatności -->
+                    <li class="myButton"><a href="#">Członkowie</a></li>
+                    <li class="myButton"><a href="#">Płatności</a></li>
+                    <!-- <li class="myButton"><a href="{{ route('register') }}">{{ __('Zarejestruj')}}</a></li> -->
+                    <li class="myButton"><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{__('Wyloguj')}}</a></li>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display:none;">
+                        @csrf 
+                        </form>
+                    @endguest
+                </ul>
+            </nav>
+        </header>
 
-        <main class="py-4">
+        <main class="container">
             @yield('content')
         </main>
     </div>
